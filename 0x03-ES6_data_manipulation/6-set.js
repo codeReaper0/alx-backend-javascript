40dd4edeb0dbc54a0033e5b9afe58d4b7b23509b
codeReaper <boluwatifetella@gmail.com>
2023-12-05 22:57:24 +0100

    data manipulation

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Tella Boluwatife <https://github.com/codeReaper0>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
