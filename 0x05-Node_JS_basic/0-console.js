2243e7941468d8e3abf12bd64629d5bba2da54f2
codeReaper <boluwatifetella@gmail.com>
2024-01-30 13:49:30 +0100

    node js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,10 @@
+/**
+ * Displays a message on the STDOUT.
+ * @param {String} msg The message to display.
+ * @author Tella Boluwatife <https://github.com/codeReaper0>
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
